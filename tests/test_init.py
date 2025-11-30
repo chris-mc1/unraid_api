7e0af9b455427a8ffdfdc054f633987e19429918
chris_mc1 <29151206+chris-mc1@users.noreply.github.com>
2025-11-30 11:42:55 +0000

    adapt init tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -3,41 +3,41 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
-from unittest.mock import ANY, AsyncMock, MagicMock
+from unittest.mock import AsyncMock, MagicMock
 
+import pytest
 from aiohttp import ClientConnectionError, ClientConnectorSSLError
-from awesomeversion import AwesomeVersion
-from custom_components.unraid_api.api import (
-    IncompatibleApiError,
-    UnraidAuthError,
-    UnraidGraphQLError,
-)
-from custom_components.unraid_api.const import DOMAIN
+from custom_components.unraid_api.api import UnraidApiClient
 from homeassistant.config_entries import ConfigEntryState
-from homeassistant.const import CONF_API_KEY, CONF_HOST
-from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from . import setup_config_entry
-from .const import MOCK_CONFIG_DATA, MOCK_OPTION_DATA
+from tests.graphql_responses import API_RESPONSES
+
+from . import add_config_entry, setup_config_entry
+from .const import (
+    API_VERSION_RESPONSE_INCOMPATIBLE,
+    RESPONSE_ERROR,
+    RESPONSE_UNAUTHENTICATED,
+)
 
 if TYPE_CHECKING:
+    from collections.abc import Awaitable, Callable
+
     from homeassistant.core import HomeAssistant
 
+    from tests.conftest import GraphqlServerMocker
 
+
+@pytest.mark.parametrize(("api_responses"), API_RESPONSES)
 async def test_load_unload_entry(
+    api_responses: dict,
     hass: HomeAssistant,
-    mock_get_api_client: AsyncMock,
+    mock_graphql_server: Callable[..., Awaitable[GraphqlServerMocker]],
 ) -> None:
     """Test setup and unload config entry."""
-    entry = await setup_config_entry(hass, data=MOCK_CONFIG_DATA, options=MOCK_OPTION_DATA)
+    mocker = await mock_graphql_server(api_responses)
+    entry = await setup_config_entry(hass, mocker)
 
     assert entry.state is ConfigEntryState.LOADED
-
-    mock_get_api_client.assert_called_once_with(
-        host=MOCK_CONFIG_DATA[CONF_HOST],
-        api_key=MOCK_CONFIG_DATA[CONF_API_KEY],
-        session=ANY,
-    )
 
     assert await hass.config_entries.async_unload(entry.entry_id)
     await hass.async_block_till_done()
@@ -47,63 +47,62 @@ async def test_load_unload_entry(
 
 async def test_load_failure(
     hass: HomeAssistant,
-    mock_get_api_client: AsyncMock,
+    monkeypatch: pytest.MonkeyPatch,
 ) -> None:
     """Test setup and unload failure."""
-    entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG_DATA, options=MOCK_OPTION_DATA)
+    mock_call_api = AsyncMock()
+    monkeypatch.setattr(UnraidApiClient, "call_api", mock_call_api, raising=True)
+    entry = add_config_entry(hass)
 
-    entry.add_to_hass(hass)
-
-    mock_get_api_client.side_effect = ClientConnectorSSLError(MagicMock(), MagicMock())
+    mock_call_api.side_effect = ClientConnectorSSLError(MagicMock(), MagicMock())
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
 
     assert entry.state is ConfigEntryState.SETUP_ERROR
     await hass.config_entries.async_unload(entry.entry_id)
-    mock_get_api_client.reset_mock()
+    mock_call_api.reset_mock()
 
-    mock_get_api_client.side_effect = TimeoutError()
+    mock_call_api.side_effect = TimeoutError()
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
 
     assert entry.state is ConfigEntryState.SETUP_RETRY
     await hass.config_entries.async_unload(entry.entry_id)
-    mock_get_api_client.reset_mock()
+    mock_call_api.reset_mock()
 
-    mock_get_api_client.side_effect = ClientConnectionError()
+    mock_call_api.side_effect = ClientConnectionError()
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
 
     assert entry.state is ConfigEntryState.SETUP_RETRY
     await hass.config_entries.async_unload(entry.entry_id)
-    mock_get_api_client.reset_mock()
+    mock_call_api.reset_mock()
 
-    mock_get_api_client.side_effect = UnraidAuthError(
-        {"errors": [{"message": "No user session found"}]}
-    )
+
+async def test_load_failure_2(
+    hass: HomeAssistant,
+    mock_graphql_server: Callable[..., Awaitable[GraphqlServerMocker]],
+) -> None:
+    """Test setup and unload failure."""
+    mocker = await mock_graphql_server(RESPONSE_UNAUTHENTICATED)
+    entry = add_config_entry(hass, mocker)
+
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
 
     assert entry.state is ConfigEntryState.SETUP_ERROR
     await hass.config_entries.async_unload(entry.entry_id)
-    mock_get_api_client.reset_mock()
 
-    mock_get_api_client.side_effect = UnraidGraphQLError(
-        {"errors": [{"message": "No user session found"}]}
-    )
+    mocker.response_set = RESPONSE_ERROR
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
 
     assert entry.state is ConfigEntryState.SETUP_ERROR
     await hass.config_entries.async_unload(entry.entry_id)
-    mock_get_api_client.reset_mock()
 
-    mock_get_api_client.side_effect = IncompatibleApiError(
-        min_version=AwesomeVersion("1.0.0"), version=AwesomeVersion("0.0.1")
-    )
+    mocker.response_set = API_VERSION_RESPONSE_INCOMPATIBLE
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
 
     assert entry.state is ConfigEntryState.SETUP_ERROR
     await hass.config_entries.async_unload(entry.entry_id)
-    mock_get_api_client.reset_mock()
